f85294e7082b1536dd1b2432b2f012867c3f762c
developeralamin <developeralaminvi@gmail.com>
2024-10-29 15:08:56 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function addToResult(value) {
+  document.getElementById("result").value += value;
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function calculateResult() {
+  try {
+    document.getElementById("result").value = eval(
+      document.getElementById("result").value
+    );
+  } catch (error) {
+    document.getElementById("result").value = "Error";
+  }
+}
